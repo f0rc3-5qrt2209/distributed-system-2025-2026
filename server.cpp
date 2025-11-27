f389341b91d413c84bf125f48ec6af6d794a3d6b
f0rc3-5qrt2209 <trunghieua10328@gmail.com>
2025-11-27 14:06:21 +0700

    commit server.cpp and client.cpp

---
+++ server.cpp
@@ -0,0 +1,40 @@
+#include <cstring>
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main(){
+    //create socket
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+                        //    IPv4,    TCP,        protocol
+
+    //specifying address
+    sockaddr_in serverAddress; //struct
+    serverAddress.sin_family = AF_INET; //declare IPv4
+    serverAddress.sin_port = htons(8080); //htons convert big edian to little edian
+    serverAddress.sin_addr.s_addr = INADDR_ANY; //listen to all connect
+
+    //binding socket
+    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    //  use this socket - import serverAddress & type cast - identify addr
+
+    //listen to the assign socket
+    listen(serverSocket, 5);
+
+    //accepting connection request
+    int clientSocket = accept(serverSocket, nullptr, nullptr);
+    //create a socket for answering, careless about client's IP and port
+
+    //receiving data
+    char buffer[1024] = {0};
+    recv(clientSocket, buffer, sizeof(buffer), 0);
+    cout << "Message from client: " << buffer << endl;
+
+    //closeing socket
+    close(serverSocket);
+
+    return 0;
+}
